0233aa4ca6f8bc1cf65ad3eef93a4944d83462b4
zhouyeshan <2455453003@qq.com>
2026-05-22 17:02:28 +0800

    fix: 可用渠道表格使用 table-fixed + colgroup 百分比列宽
    所有列使用百分比宽度(10%/12%/8%/30%/40%)确保5列都在可视范围内，
    支持模型列占40%空间允许内容自然换行，无需水平滚动。

--- frontend/src/components/channels/AvailableChannelsTable.vue
+++ frontend/src/components/channels/AvailableChannelsTable.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="card overflow-x-auto">
-    <table class="w-full border-collapse text-sm">
+  <div class="card overflow-hidden">
+    <table class="w-full table-fixed border-collapse text-sm">
+      <colgroup>
+        <col style="width: 10%" />
+        <col style="width: 12%" />
+        <col style="width: 8%" />
+        <col style="width: 30%" />
+        <col style="width: 40%" />
+      </colgroup>
       <thead>
         <tr class="border-b border-gray-100 bg-gray-50/50 text-xs font-medium uppercase tracking-wide text-gray-500 dark:border-dark-700 dark:bg-dark-800/50 dark:text-gray-400">
-          <th class="min-w-[120px] whitespace-nowrap px-4 py-3 text-center">{{ columns.name }}</th>
-          <th class="min-w-[140px] whitespace-nowrap px-4 py-3 text-left">{{ columns.description }}</th>
-          <th class="min-w-[100px] whitespace-nowrap px-4 py-3 text-left">{{ columns.platform }}</th>
-          <th class="min-w-[200px] whitespace-nowrap px-4 py-3 text-left">{{ columns.groups }}</th>
-          <th class="min-w-[200px] whitespace-nowrap px-4 py-3 text-left">{{ columns.supportedModels }}</th>
+          <th class="px-4 py-3 text-center">{{ columns.name }}</th>
+          <th class="px-4 py-3 text-left">{{ columns.description }}</th>
+          <th class="px-4 py-3 text-left">{{ columns.platform }}</th>
+          <th class="px-4 py-3 text-left">{{ columns.groups }}</th>
+          <th class="px-4 py-3 text-left">{{ columns.supportedModels }}</th>
         </tr>
       </thead>
       <tbody v-if="loading">
